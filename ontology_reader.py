c521f9d0bb81f728e627918a6c802b0416cac093
Kunal Rout <2kunal6@gmail.com>
2020-12-23 06:26:49 +0100

    Implementing BFS

--- ontology_reader.py
+++ ontology_reader.py
@@ -1,5 +1,6 @@
 from owlready2 import *
 from util import file_util
+from util.graph_util import Graph
 from service import python_content_creator
 
 #onto = get_ontology("https://github.com/2kunal6/SemanticWebLab/blob/master/ml-hierarchy.owl")
@@ -9,6 +10,8 @@ onto.load()
 
 print(list(onto.classes()))
 
+g = Graph()
+
 for onto_class in list(onto.classes()):
     ontology_class = str(onto_class)
     if (ontology_class == "ml-hierarchy.MachineLearningAlgorithms"):
@@ -16,7 +19,17 @@ for onto_class in list(onto.classes()):
             algorithms_onto_class = str(algorithms_ontology_class)
             algorithms_onto_class = algorithms_onto_class.replace('ml-hierarchy.', '')
             filename = 'ml_algorithms/' + algorithms_onto_class + '.py'
-            file_util.create_file(filename)
+
+            #file_util.create_file(filename)
             parent_classes = onto.get_parents_of(algorithms_ontology_class)
-            file_util.append_to_file(filename, python_content_creator.create_class(algorithms_onto_class, list(parent_classes)))
+            if(not parent_classes):
+                continue
+
+            parent_class = str(parent_classes[0])
+            #file_util.append_to_file(filename, python_content_creator.create_class(algorithms_onto_class, list(parent_classes)))
+
+            print(parent_class.replace('ml-hierarchy.', ''), ' ', algorithms_onto_class)
+            g.addEdge(parent_class.replace('ml-hierarchy.', ''), algorithms_onto_class)
         break
+print('starting BFS')
+print(g.BFS('MachineLearningAlgorithms'))
