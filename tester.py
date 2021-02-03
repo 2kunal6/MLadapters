1b4e694e247002160aba5b662616cd7fb33ba1d8
Kunal Rout <2kunal6@gmail.com>
2021-02-03 14:34:28 +0100

    Checking DT

--- tester.py
+++ tester.py
@@ -1,13 +1,12 @@
 from MachineLearningAlgorithms.Classification.DecisionTree.DecisionTree import DecisionTree
 from sklearn.datasets import load_iris
 from sklearn import tree
-
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 dt = DecisionTree()
 X, y = load_iris(return_X_y = True)
-model = dt.fit(X, y, None, None)
-print(model)
-tree.plot_tree(model)
-tree.export_graphviz(model,
-                     out_file="tree.dot",
-                     filled = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+model = dt.fit(X_train, y_train, None, None)
+y_pred = model.predict(X_test)
+print("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
